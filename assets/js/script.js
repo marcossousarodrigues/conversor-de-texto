2a348804f4b988da4d9c7d8f0bc67635718f7369
Marcos Rodrigues <marcosrodrigues3546@gmail.com>
2024-03-09 13:06:17 -0300

    Projeto Conversor de Texto
    Adicionando projeto web desenvolvido em HTML, CSS e JavasCript.

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+
+class Conversor{
+    selecionaElementos(){
+        const textareaConversor = document.querySelector('.textarea-conversor');
+        const conversorMaiuscula = document.querySelector('.conversor-maiuscula');
+        const conversorMinuscula = document.querySelector('.conversor-minuscula');
+        const conversorPrimeiraLetra = document.querySelector('.conversor-primeira-letra');
+        const valorConvertido = document.querySelector('.valor-convertido');
+        
+        return {
+                textareaConversor, 
+                btnMaiuscula: conversorMaiuscula, 
+                btnMinuscula: conversorMinuscula, 
+                btnPrimeiraLetra: conversorPrimeiraLetra,
+                valorConvertido
+            };
+    }
+
+    eventos () {
+        const el = this.selecionaElementos();
+        el.btnMaiuscula.addEventListener('click', (e)=>{
+            e.preventDefault();
+            el.valorConvertido.innerText = el.textareaConversor.value.toUpperCase();
+        });
+        el.btnMinuscula.addEventListener('click', (e)=>{
+            e.preventDefault();
+            el.valorConvertido.innerText = el.textareaConversor.value.toLowerCase();
+        });
+        el.btnPrimeiraLetra.addEventListener('click', (e)=>{
+            e.preventDefault();
+            const listaPalavras = el.textareaConversor.value.split(" ");
+            let palavraConvertida = "";
+            listaPalavras.map((item)=>{
+                palavraConvertida += item[0].toUpperCase() + item.substring(1)+" ";
+            })
+            console.log(palavraConvertida)
+            el.valorConvertido.innerText = palavraConvertida;
+        });
+    }
+}
+
+const con = new Conversor();
+con.eventos();
